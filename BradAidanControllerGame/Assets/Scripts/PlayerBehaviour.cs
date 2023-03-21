a38b8caecaefd8fe1242c63019573fc68db6348a
KarmicSine68 <117841894+KarmicSine68@users.noreply.github.com>
2023-03-21 10:15:06 -0500

    Added a script

---
+++ BradAidanControllerGame/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,25 @@
+/*****************************************************************************
+// File Name :         PlayerBehaviour.cs
+// Author :            Brad Dixon
+// Creation Date :     March 21st, 2023
+//
+// Brief Description : Movement for the player
+*****************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
