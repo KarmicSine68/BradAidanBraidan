7f0ecbe7a59cc221207c8067009e10eadeb1c76a
Sleepy2004 <aratcliffe@mail.bradley.edu>
2023-04-09 20:48:17 -0500

    AttackChanges

--- BradAidanControllerGame/Assets/Scripts/AttackScript.cs
+++ BradAidanControllerGame/Assets/Scripts/AttackScript.cs
@@ -1,9 +1,23 @@
+/*****************************************************************************
+// File Name :         AttackScript.cs
+// Author :            Aidan Ratcliffe
+// Creation Date :     April 9th, 2023
+//
+// Brief Description : Attacking for the player
+*****************************************************************************/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AttackScript : MonoBehaviour
 {
+    public PlayerBehaviour player;
+    public GameObject RightAttackArea;
+    public GameObject LeftAttackArea;
+    public bool HasAttacked;
+    public float timer;
+
+
     // Start is called before the first frame update
     void Start()
     {
