801314e64408553f557f044d934099250ad2e4c1
Sleepy2004 <aratcliffe@mail.bradley.edu>
2023-04-12 21:31:22 -0500

    Magic stuff

--- BradAidanControllerGame/Assets/Scripts/Classes/Mage.cs
+++ BradAidanControllerGame/Assets/Scripts/Classes/Mage.cs
@@ -5,21 +5,59 @@
 //
 // Brief Description : Data for the mage
 *****************************************************************************/
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Mage : ClassChanger
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 target;
+    public GameObject player;
+    public GameObject MagicPrefab;
+    PlayerControls controls;
+
+    public float ProjectileSpeed;
+
+    private void Awake()
     {
-        
+        controls = new PlayerControls();
+
+        controls.PlayerActions.Light.performed += ctx => Light();
+    }
+
+    private void Light()
+    {
+        Vector3 difference = target - player.transform.position;
+        //float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        //player.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ - 90);
+        float distance = difference.magnitude;
+        Vector2 direction = difference / distance;
+        direction.Normalize();
+        ShootMagic(direction);
+    }
+
+    void ShootMagic(Vector2 direction)
+    {
+        GameObject m = Instantiate(MagicPrefab) as GameObject;
+        m.transform.position = player.transform.position;
+        m.GetComponent<Rigidbody2D>().velocity = direction * ProjectileSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnEnable()
+    {
+        controls.PlayerActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.PlayerActions.Disable();
     }
 }
--- BradAidanControllerGame/Assets/Scripts/MagicBehavior.cs
+++ BradAidanControllerGame/Assets/Scripts/MagicBehavior.cs
@@ -1,18 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MagicBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
+        if (other.tag == "Enemy")
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(other.gameObject);
+        //Destroy(this.gameObject);
     }
 }
