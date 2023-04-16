7fd4af878d06c8915990f4bb73e496e598a485e8
Sleepy2004 <aratcliffe@mail.bradley.edu>
2023-04-16 18:31:11 -0500

    changes

--- BradAidanControllerGame/Assets/Scripts/AttackScript.cs
+++ BradAidanControllerGame/Assets/Scripts/AttackScript.cs
@@ -5,28 +5,73 @@
 //
 // Brief Description : Attacking for the player
 *****************************************************************************/
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AttackScript : MonoBehaviour
 {
-    public PlayerBehaviour player;
+    public Ranger Rplayer;
     public GameObject RightAttackArea;
     public GameObject LeftAttackArea;
+    PlayerControls controls;
     public bool HasAttacked;
     public float timer;
 
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        controls = new PlayerControls();
+
+        controls.PlayerActions.Light.performed += ctx => Light();
+    }
+
+    private void Light()
+    {
+        Attack();
+    }
+
+    public void Attack()
+    {
+        if (HasAttacked == true)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        if (timer <= 0)
+        {
+            timer = 2f;
+            HasAttacked = false;
+            RightAttackArea.SetActive(false);
+            LeftAttackArea.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void RightAttack()
+    {
+        RightAttackArea.SetActive(true);
+        HasAttacked = true;
+    }
+
+    public void LeftAttack()
+    {
+        LeftAttackArea.SetActive(true);
+        HasAttacked = true;
+    }
+    private void OnEnable()
+    {
+        controls.PlayerActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.PlayerActions.Disable();
     }
 }
