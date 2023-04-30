d6955b623a271d1d5f156fd54781fa185436991d
Sleepy2004 <aratcliffe@mail.bradley.edu>
2023-04-30 18:54:16 -0500

    CameraBorderchanges

---
+++ BradAidanControllerGame/Assets/Scripts/UI/BorderBehavior.cs
@@ -0,0 +1,30 @@
+/*****************************************************************************
+// File Name :         BorderBehavior.cs
+// Author :            Aidan Ratcliffe
+// Creation Date :     April 30th, 2023
+//
+// Brief Description : Spawns a border/bounding box whenever enemies approach
+*****************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderBehavior : MonoBehaviour
+{
+    public GameObject CameraBorder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraBorder.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if ()
+        //{
+
+        //}
+    }
+}
