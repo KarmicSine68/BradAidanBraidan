5c22a9b12e46f0b9057e752ff4a0449b4b15e318
KarmicSine68 <117841894+KarmicSine68@users.noreply.github.com>
2023-05-05 19:57:53 -0500

    Enemies now go after the player
    They still need to attack

--- BradAidanControllerGame/Assets/Scripts/Enemies/BradEnemyBehaviour.cs
+++ BradAidanControllerGame/Assets/Scripts/Enemies/BradEnemyBehaviour.cs
@@ -11,15 +11,17 @@ using UnityEngine;
 
 public class BradEnemyBehaviour : MonoBehaviour
 {
-    //The player game object is used so the enemies move towards them
-    [SerializeField] GameObject player;
+    //Gets an array of the players and assigns one of them as the target
+    private GameObject[] player = new GameObject[2];
+    private GameObject target;
 
     private bool inRange;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        player = GameObject.FindGameObjectsWithTag("Player");
+        AssignTarget();
     }
 
     //Enemies move towards players until one of them is range
@@ -28,7 +30,47 @@ public class BradEnemyBehaviour : MonoBehaviour
         if (!inRange)
         {
             transform.position = Vector3.MoveTowards(transform.position,
-                player.transform.position, 2f * Time.deltaTime);
+                target.transform.position, 2f * Time.deltaTime);
+        }
+    }
+
+    private void AssignTarget()
+    {
+        //Local variables for both players
+        GameObject t1;
+        GameObject t2;
+
+        //Used to record the distance of the players from the enemy
+        float distance = 0;
+
+        foreach(GameObject ctx in player)
+        {
+            //The x and y positions of the player and enemy
+            //x1 and y1 are the enemy, x2 and y2 are the player
+            float x1 = gameObject.transform.position.x;
+            float x2 = ctx.transform.position.x;
+            float y1 = gameObject.transform.position.y;
+            float y2 = ctx.transform.position.y;
+
+            //The sum that will be squarerooted to find distance
+            float sum;
+
+            //Fun math time. Using the distance formulat to find the 
+            sum = (Mathf.Pow(x2 - x1, 2) + Mathf.Pow(y2 - y1, 2));
+
+            sum = Mathf.Sqrt(sum);
+
+            if(distance == 0)
+            {
+                distance = sum;
+                target = ctx;
+            }
+            //If the new distance is closer, assign that as the target instead
+            else if(distance >= sum)
+            {
+                distance = sum;
+                target = ctx;
+            }
         }
     }
 
@@ -41,6 +83,7 @@ public class BradEnemyBehaviour : MonoBehaviour
         if(collision.CompareTag("Player"))
         {
             inRange = true;
+            StartCoroutine(Attack());
         }
     }
 
@@ -53,6 +96,20 @@ public class BradEnemyBehaviour : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             inRange = false;
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        yield return new WaitForSeconds(1.5f);
+        if(inRange)
+        {
+            Debug.Log("Successfully attacked");
+            StartCoroutine(Attack());
+        }
+        else
+        {
+            Debug.Log("Missed");
         }
     }
 }
